f0f678b00dc0ff72dac1776cdccd09e2ce57e14b
altangerel <altangerelg203@gmail.com>
2025-03-22 05:02:22 +0800

    unit

--- api/src/index.js
+++ api/src/index.js
@@ -177,6 +177,7 @@ app.get("/recordsGet", async (req, res) => {
 });
 
 const port = 3002;
+app.use("/api/v1", PostRoute);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
